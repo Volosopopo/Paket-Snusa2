c3afe7216567195a865258188d482aad0872978b
Volosopopo <124680857+Volosopopo@users.noreply.github.com>
2023-02-14 14:37:52 +0300

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,12 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = "No"
+big_netseloe=tseloe * 3.5 
+drobnoe=drobnoe - 1 
+stroka = stroka * 2+ "_" + "Yes" * 3
+print (tseloe/drobnoe)
+print (big_netseloe/drobnoe)
+print (stroka)
+print (big_netseloe)
+print (drobnoe)
+print (tseloe)
---
+++ main (6).py
@@ -0,0 +1,16 @@
+a=7
+b=14
+c=a+b
+d='Привет'
+g='Андрей'
+print((a>b) and (b>a))
+print((b>a)and(a<b))
+print((a>b) or (b>a))
+print(not(b>a))
+print((c>b) or (b>a))
+print((a>c)or(b>c))
+print((c<b)or(b<a))
+print((a<c)or(b>a))
+print((d>g)and(g>d))
+print((g>d)or(g<d))
+print(not(d<g))
