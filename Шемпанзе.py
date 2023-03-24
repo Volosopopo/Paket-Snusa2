31bf8db60879e903533dac79ea021aff4251285d
Volosopopo <124680857+Volosopopo@users.noreply.github.com>
2023-03-24 10:34:36 +0300

    дз на 24.03

---
+++ Шемпанзе.py
@@ -0,0 +1,8 @@
+a=int(input())
+b=a%10
+#второе
+c=(a-b)/10
+print(b,c)
+#перво
+f=(b*10)+c
+print(f)
---
+++ обезьян.py
@@ -0,0 +1,11 @@
+x=int(input())
+k=int(input())
+if k<x:
+    y=k*x
+    print(y)
+elif k>x:
+    y=k+x
+    print(y)
+if k==x:
+    y=k/x
+    print(y)
